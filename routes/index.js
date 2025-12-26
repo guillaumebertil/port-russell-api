16d1d19d0ad4ebba0416a6add6d1d1e273c4b7f3
guillaumebertil <guillaume.bertil@gmail.com>
2025-12-26 20:42:53 +0100

    feat: routes login, logout et users

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const authService = require('../services/auth')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/**
+ * GET / - Page d'accueil
+ */
+router.get('/', (req, res) => {
+    res.send('API Port de Plaisance Russell');
 });
+
+/**
+ * POST /login - Authentification
+ */
+router.post('/login', authService.login);
+
+/**
+ * GET /logout - Déconnexion
+ */
+router.get('/logout', authService.logout);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express     = require('express');
+const router      = express.Router();
+const userService = require('../services/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/**
+ * GET /users - Récupérer tous les utilisateurs
+ */
+router.get('/', userService.getAllUsers);
+
+/**
+ * GET /users/:email - Récupérer un utilisateur par email
+ */
+router.get('/:email', userService.getUserByEmail);
+
+/**
+ * POST /users - Créer un utilisateur
+ */
+router.post('/', userService.createUser);
+
+/**
+ * PUT /users/:email - Mettre à jour un utilisateur
+ */
+router.put('/:email', userService.updateUser);
+
+/**
+ * DELETE /users/:email - Supprimer un utilisateur
+ */
+router.delete('/:email', userService.deleteUser);
 
 module.exports = router;
