cd957fe029ed7b06c31a178ad2b364b3106e3336
guillaumebertil <guillaume.bertil@gmail.com>
2026-01-02 11:47:07 +0100

    fix: bug empêchant l'accès à la documentation

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use('/api/catways', catwaysRouter)
 app.use('/api/reservations', reservationsRouter);
 
 // Swagger
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 // Gestion erreur 404
 app.use((req, res, next) => {
